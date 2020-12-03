49b64d0b9e4b2dd25a30a9ce30525b8f9885bb75
wuhanbin <wuhanbin@gmail.com>
2020-12-03 18:55:21 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cat <<-EOF > /etc/xfly/conf
 {
-  "inbounds": [
+  "${IN}": [
   {
     "port": ${PORT},
     "protocol": "${PROTOCOL}",
@@ -23,7 +23,7 @@ cat <<-EOF > /etc/xfly/conf
     }
   }
   ],
-  "outbounds": [
+  "${OUT}": [
   {
     "protocol": "freedom"
   }
